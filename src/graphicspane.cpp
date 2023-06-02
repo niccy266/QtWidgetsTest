bbd0c84b4f416539e9bc561152ad0f4d9b6db3ae
Nicolás Concha Vargas <niccy266@gmail.com>
2023-06-02 14:19:58 -0400

    Generated a project with QtCreator

---
+++ src/graphicspane.cpp
@@ -0,0 +1,20 @@
+#include <SFML/Graphics.hpp>
+#include <Qwidget>
+
+class graphicspane
+{
+private:
+   /* data */
+public:
+   public QWidget, public sf::RenderWindow
+   graphicspane(/* args */);
+   ~graphicspane();
+};
+
+graphicspane::graphicspane(/* args */)
+{
+}
+
+graphicspane::~graphicspane()
+{
+}
